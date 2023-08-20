5e812fd782d6fe9133828a0eef4375ccfd3da7af
Dolu <doludev@protonmail.com>
2023-08-20 10:17:35 +0200

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import NDK from "@nostr-dev-kit/ndk";
+
+const ndk = new NDK({
+  explicitRelayUrls: ["ws://localhost:29100"],
+});
+
+async function start() {
+  await ndk.connect().catch((err) => console.log("connect() catch", err));
+
+  const subOptions = { closeOnEose: false };
+
+  const notesSub = ndk.subscribe({}, subOptions);
+  notesSub.on("event", async (event) => {
+    console.log("notes event", event);
+  });
+}
+
+start().catch((err) => {
+    console.log("start() catch", err)
+});
